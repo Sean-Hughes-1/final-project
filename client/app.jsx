f0f81f1a3364b3e3ea175727fb3c0e75c9619763
Sean Hughes <coronapilot69@gmail.com>
2023-03-05 04:20:26 +0000

    issue 3 is complete - user can add revenues

--- client/app.jsx
+++ client/app.jsx
@@ -3,7 +3,9 @@ import Home from './pages/home';
 import Profile from './pages/profile';
 import NotFound from './pages/not-found';
 import Expenses from './pages/expenses';
+import RevenueForm from './pages/revenue-form';
 import ExpenseForm from './pages/expense-form';
+import Revenues from './pages/revenues';
 import { parseRoute } from './lib';
 export default function App() {
   const[route, setRoute] = useState(parseRoute(window.location.hash));
@@ -22,6 +24,12 @@ export default function App() {
   }
   if (route.path === 'expense-form') {
     return <ExpenseForm />
+  }
+  if (route.path === 'revenue-form') {
+    return <RevenueForm />
+  }
+  if (route.path === 'revenues') {
+    return <Revenues />
   }
   return <NotFound />;
 }
---
+++ client/pages/revenues.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RevenueAdd from '/workspaces/final-project/client/components/revenue-add';
+import Footer from '../components/footer';
+export default function Revenues(props) {
+  return (
+    <div>
+      <h1>Hello, world! I am the Revenues page</h1>
+      <RevenueAdd />
+      <br></br>
+      <Footer />
+    </div>
+  );
+}
